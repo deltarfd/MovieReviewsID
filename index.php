45b421bf89a698109fc899d071e972caedb2dd86
Delta R F D <deltarfd@live.com>
2018-12-08 22:25:49 +0700

    Add Script Reply

--- index.php
+++ index.php
@@ -54,7 +54,28 @@
         }
      
         // kode aplikasi nanti disini
-     
+        $data = json_decode($body, true);
+        if(is_array($data['events'])){
+            foreach ($data['events'] as $event)
+            {
+                if ($event['type'] == 'message')
+                {
+                    $result = $bot->replyText($event['replyToken'], $event['message'];
+                    return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+                    // if($event['message']['type'] == 'text')
+                    // {
+                    //     // send same message as reply to user
+                    //     $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+         
+                    //     // or we can use replyMessage() instead to send reply message
+                    //     // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    //     // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+         
+                        
+                    // }
+                }
+            } 
+        }
     });
      
     $app->run();
